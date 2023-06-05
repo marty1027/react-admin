614c5347d8530e8cb027b3ea8ca38948a6361aea
Finja <dp.dm17@gmail.com>
2023-06-05 14:45:31 -0300

    Atualização de projeto sidebar e topbar

---
+++ src/data/mockData.js
@@ -0,0 +1,50 @@
+export const mockDataGroup = [
+    {
+        id: 1,
+        nome: "Fulano", 
+        idade: 22,
+        email: "fulano@gmail.com",
+        tel: "(21) 12212-3223",
+        acesso: "admin"
+    },
+    {
+        id: 2,
+        nome: "Ciclano", 
+        idade: 41,
+        email: "Ciclano@gmail.com",
+        tel: "(21) 12212-3223",
+        acesso: "manager"
+    },
+    {
+        id: 3,
+        nome: "Beltrano", 
+        idade: 31,
+        email: "Beltrano@gmail.com",
+        tel: "(21) 12212-3223",
+        acesso: "user"
+    },
+    {
+        id: 4,
+        nome: "Alice", 
+        idade: 22,
+        email: "Alice@gmail.com",
+        tel: "(21) 12212-3223",
+        acesso: "user"
+    },
+    {
+        id: 5,
+        nome: "Beth", 
+        idade: 43,
+        email: "Beth@gmail.com",
+        tel: "(21) 12212-3223",
+        acesso: "user"
+    },
+    {
+        id: 6,
+        nome: "Bento", 
+        idade: 38,
+        email: "Bento@gmail.com",
+        tel: "(21) 12212-3223",
+        acesso: "user"
+    },
+]
